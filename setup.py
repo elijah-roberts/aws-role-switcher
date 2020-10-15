43effa9419026ab38274b558d51c7cf548347959
Elijah Roberts <ejr@kount.com>
2020-10-14 19:17:19 -0600

    Update version 1.1.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='aws-role-switcher',
-    version='1.0.1',
+    version='1.1.0',
     py_modules=['aws-role-switcher'],
     author="Elijah Roberts",
     author_email="elijah@elijahjamesroberts.com",
